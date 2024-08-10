128cfaaacbdfe47e8c6ef249bd2fda91647b23b2
Yuvraj Tharu <124166790+Yuvraj-Tharu@users.noreply.github.com>
2024-08-10 19:42:38 +0545

    Update weatherscript.js

--- weatherscript.js
+++ weatherscript.js
@@ -1,92 +1,111 @@
+function weatherData(cityName) {
+  const apiKey = "4b5c9e30be2d6330c99893c75144aba1";
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      const now = new Date();
+      document.getElementById("demoday").innerHTML = days[now.getDay()];
+      document.getElementById("demomonth").innerHTML = months[now.getMonth()];
+      document.getElementById("demodate").innerHTML = now.getDate();
+      document.getElementById("demoyear").innerHTML = now.getFullYear();
 
+      console.log(response);
+      document.querySelector(".city").innerText = response.name;
+      document.querySelector(".description").innerText =
+        response.weather[0].description;
 
+      document.querySelector(".temp").innerHTML =
+        response.main.temp.toFixed(1) + "°C";
 
-// fetches data from the local host php server with the help of one parameter : city
-function weatherData(name) {
-    
-        fetch("https://api.openweathermap.org/data/2.5/weather?lat=27.58&lon=85.50&appid=4b5c9e30be2d6330c99893c75144aba1&units=metric" + name)
-        .then(response => response.json())
-        .then(response => {
+      document.querySelector(".iconurl").src =
+        "https://openweathermap.org/img/wn/" +
+        response.weather[0].icon +
+        "@4x.png";
 
-              const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-              const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-              const da = new Date();
-              const m = new Date();
-              const d = new Date();
-              const y = new Date();
-              let day = days[da.getDay()];
-              let month = months[m.getMonth()];
-              document.getElementById("demoday").innerHTML = day;
-              document.getElementById("demomonth").innerHTML = month;
-              document.getElementById("demodate").innerHTML = d.getDate();
-              document.getElementById("demoyear").innerHTML = y.getFullYear();
-              console.log(response);
-              // displays the name of the city from search bar
-              document.querySelector(".city").innerText = name;
-  // displays the weather description of the city mentioned in the search bar
-              document.querySelector(".description").innerText = response.weather[0].description;
+      document.querySelector(".pressure").innerText =
+        response.main.pressure + " mbar";
+      document.querySelector(".humidity").innerText =
+        response.main.humidity + " %";
+      document.querySelector(".wind").innerText = response.wind.speed + " km/h";
 
-              // displays the temperature of the city mentioned in the search bar\
-                const kelvinTemperature = response.main.temp;
-                const celsiusTemperature = kelvinTemperature - 273.15;
+      // Store data in the local storage
+      localStorage.city = response.name;
+      localStorage.description = response.weather[0].description;
+      localStorage.temp = response.main.temp + "°C";
+      localStorage.iconurl =
+        "https://openweathermap.org/img/wn/" +
+        response.weather[0].icon +
+        "@4x.png";
+      localStorage.pressure = response.main.pressure + " mbar";
+      localStorage.humidity = response.main.humidity + " %";
+      localStorage.wind = response.wind.speed + " km/h";
+      localStorage.when = Date.now();
+    })
+    .catch((err) => {
+      console.error("Error fetching weather data:", err);
+    });
+}
 
-                document.querySelector(".temp").innerHTML = celsiusTemperature.toFixed(1) + "°C";
-                //  document.querySelector(".temp").innerHTML = response.main.temp + "°F";
-              // displays an image which signifies the weather of the city 
-              document.querySelector(".iconurl").src ="https://openweathermap.org/img/wn/"+  response.weather[0].icon +"@4x.png";
-              // displays the pressure of the city mentioned in the search bar in hPa
-              document.querySelector(".pressure").innerText = response.main.pressure + "    mbar";
-              // displays the humidity of the city in percentage
-              document.querySelector(".humidity").innerText = response.main.humidity + "    %";
-              // displays the wind speed in m/s
-              document.querySelector(".wind").innerText = response.wind.speed + "    km/h";
-            //store datas in the local storage
-              localStorage.city= name;
-              localStorage.description=response.Description;
-              localStorage.temp=response.Temperature + "°C";
-              localStorage.iconurl=response.Icon;
-              localStorage.pressure=response.Pressure + "    mbar";
-              localStorage.humidity= response.Humidity + "    %";
-              localStorage.wind= response.Wind_speed + "    km/h";
-              localStorage.when=Date.now();
-            })
+function cityName() {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("sw.js").then(
+        function (registration) {
+          console.log("ServiceWorker registration successful");
+        },
+        function (err) {
+          console.log("ServiceWorker registration failed:", err);
+        }
+      );
+    });
+  }
 
-            .catch(err =>{
-                console.log(err);
-            })
+  // Retrieve the city name from the search bar
+  let city = document.querySelector(".search-bar").value;
+  if (city === "") {
+    city = "panauti"; // Default city if search bar is empty
+  }
 
-};
+  // Call the weatherData function with the city name
+  weatherData(city);
+}
 
-function cityName(){
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('sw.js').then(function(registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful');
-            }, function(err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-            });
-        });
-    }
-     // stores the text content of the search bar in city when the button is pressed
-    let city =document.querySelector(".search-bar").value;
-    if (city == ""){
-        city = "panauti";
-    }
-    // calls the weatherData function and takes one parameter: city
-    weatherData(city);
-
-
-};
-// data is shown when enter key is pressed
+// Initial call to display weather data for the default or entered city
 cityName();
+
+// Event listener for Enter key press in the search bar
 document
-      .querySelector(".search-bar")
-      .addEventListener("keyup", function(event) {
-          if (event.key == "Enter") {
-              // data is shown when enter key is pressed
-              cityName();
-          }
-      });
-  // this function is called when the search button is clicked
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      cityName();
+    }
+  });
+
+// Event listener for the search button click
+document.querySelector(".search-button").addEventListener("click", cityName);
